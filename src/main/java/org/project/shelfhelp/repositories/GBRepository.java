4d60eaf455452c6cca41df6997bbd67a926e37af
Anna Arnold <anna.arnold.96@gmail.com>
2024-11-04 14:20:39 +0000

    added get by author and title

--- src/main/java/org/project/shelfhelp/repositories/GBRepository.java
+++ src/main/java/org/project/shelfhelp/repositories/GBRepository.java
@@ -10,9 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GBRepository {
-    static String generalUrl = "https://books.google.com/books"; // Amend once you know the correct link
-    static String apiKey = "AIzaSyDVEnipudTAkK1cFIw1Tuy7I4vRbqh8WUw"; // Don't think this is needed
-    static String id = "buc0AAAAMAAJ"; // This is an example for now. Replace with general fed in ID
+
+//    static String id = "buc0AAAAMAAJ"; // This is an example for now
 
     public static Book getABookbyId(String id) throws Exception {
 
@@ -26,18 +25,17 @@ public class GBRepository {
         JsonNode bookTree = mapper.readTree(book);
 
         // FOR DEBUGGING
-     System.out.println(bookTree);
+//     System.out.println(bookTree);
 
         JsonNode volumeInfo = bookTree.get("volumeInfo");
         JsonNode imageLinks = volumeInfo.get("imageLinks");
 
         String title = String.valueOf(volumeInfo.get("title"));
         String author = String.valueOf(volumeInfo.get("authors"));
-        int year = Integer.parseInt(String.valueOf(volumeInfo.get("publishedDate")).replace("\"",""));
+        String year = (String.valueOf(volumeInfo.get("publishedDate")).replace("\"",""));
         String summary = String.valueOf(volumeInfo.get("description"));
-//        String genre = String.valueOf(volumeInfo.get("authors")); *** THERE IS NO GENRE FIELD ***
         String bookCover = String.valueOf(imageLinks.get("thumbnail"));
-        Float averagePublicRating = Float.valueOf(String.valueOf(volumeInfo.get("averageRating")));
+        float averagePublicRating = (volumeInfo.get("averageRating") == null) ? 0 : Float.parseFloat(String.valueOf(volumeInfo.get("averageRating")));
 
         System.out.println("title: " + title);
         System.out.println("author: " + author);
@@ -46,58 +44,85 @@ public class GBRepository {
         System.out.println("book cover: " + bookCover);
         System.out.println("rating: " + averagePublicRating);
 
-
+//            return null;
         return new Book(id, title, author, year, averagePublicRating, summary, bookCover);
     }
 
-//    public static List <Book> getBooksByTitle(String title) throws Exception {
-//
-//        String url = "https://www.googleapis.com/books/v1/volumes/?q=" + title;
-//
-//        var response = Unirest.get(url).header("Accept", "application.json").asString();
-//
-//        String book = response.getBody();
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        JsonNode bookTree = mapper.readTree(book);
-//
-////        // FOR DEBUGGING
-////        System.out.println(bookTree);
-//        var books = bookTree.get("items"); // Gets an individual book
-//        var individualBook = bookTree.get("items").get(2); // Gets an individual book
-//        List<Book> bookList = new ArrayList<>();
-//
-//        for (JsonNode i: books){
-//            JsonNode volumeInfo = bookTree.get("volumeInfo");
-//            JsonNode imageLinks = volumeInfo.get("imageLinks");
-//
-//            String id = String.valueOf(volumeInfo.get("id"));
-//            String author = String.valueOf(volumeInfo.get("authors"));
-//            int year = Integer.parseInt(String.valueOf(volumeInfo.get("publishedDate")).replace("\"",""));
-//            String summary = String.valueOf(volumeInfo.get("description"));
-//            String bookCover = String.valueOf(imageLinks.get("thumbnail"));
-//            Float averagePublicRating = Float.valueOf(String.valueOf(volumeInfo.get("averageRating")));
-//
-//
-//
-//            System.out.println("title: " + title);
-//            System.out.println("author: " + author);
-//            System.out.println("year: " + year);
-//            System.out.println("summary: " + summary);
-//            System.out.println("book cover: " + bookCover);
-//            System.out.println("rating: " + averagePublicRating);
-//
-//        }
-//
-//        System.out.println(books);
-//        System.out.println(individualBook);
-//
-//
+    public static List <Book> getBooksByTitle(String title) throws Exception {
+
+        String url = "https://www.googleapis.com/books/v1/volumes/?q=" + title;
+
+        var response = Unirest.get(url).header("Accept", "application.json").asString();
+
+        String book = response.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode bookTree = mapper.readTree(book);
+
+//        // FOR DEBUGGING
+//        System.out.println(bookTree);
+        var books = bookTree.get("items"); // Gets an individual book
+        var individualBook = bookTree.get("items").get(2); // Gets an individual book
+        List<Book> bookList = new ArrayList<>();
+
+        for (JsonNode i: books){
+            JsonNode volumeInfo = i.get("volumeInfo");
+            JsonNode imageLinks = volumeInfo.get("imageLinks");
+//            System.out.println(volumeInfo);
+
+            String id = String.valueOf(volumeInfo.get("id"));
+            String author = String.valueOf(volumeInfo.get("authors"));
+            String year = (String.valueOf(volumeInfo.get("publishedDate")).replace("\"",""));
+            String summary = String.valueOf(volumeInfo.get("description"));
+            String bookCover = String.valueOf(imageLinks.get("thumbnail"));
+            float averagePublicRating = (volumeInfo.get("averageRating") == null) ? 0 : Float.parseFloat(String.valueOf(volumeInfo.get("averageRating")));
+
+            bookList.add(new Book(id,title,author,year,averagePublicRating,summary,bookCover));
+
+        }
 //        return null;
-//    }
+        return bookList;
+    }
+
+    public static List <Book> getBooksByAuthor(String author) throws Exception {
+
+        String url = "https://www.googleapis.com/books/v1/volumes/?q=inauthor:" + author;
+
+        var response = Unirest.get(url).header("Accept", "application.json").asString();
+
+        String book = response.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode bookTree = mapper.readTree(book);
+
+//        // FOR DEBUGGING
+//        System.out.println(bookTree);
+        var books = bookTree.get("items"); // Gets an individual book
+        var individualBook = bookTree.get("items").get(2); // Gets an individual book
+        List<Book> bookList = new ArrayList<>();
+
+        for (JsonNode i: books){
+            JsonNode volumeInfo = i.get("volumeInfo");
+            JsonNode imageLinks = volumeInfo.get("imageLinks");
+//          System.out.println(volumeInfo);
+
+            String id = String.valueOf(volumeInfo.get("id"));
+            String title = String.valueOf(volumeInfo.get("title"));
+            String year = (String.valueOf(volumeInfo.get("publishedDate")).replace("\"",""));
+            String summary = String.valueOf(volumeInfo.get("description"));
+            String bookCover = String.valueOf(imageLinks.get("thumbnail"));
+            float averagePublicRating = (volumeInfo.get("averageRating") == null) ? 0 : Float.parseFloat(String.valueOf(volumeInfo.get("averageRating")));
+
+            bookList.add(new Book(id,title,author,year,averagePublicRating,summary,bookCover));
+
+        }
+        return bookList;
+    }
 //public static void main(String[] args) throws Exception {
-//    GBRepository.getABookbyId("buc0AAAAMAAJ");
-//    GBRepository.getBooksByTitle("Alice in borderland");
+////    GBRepository.getABookbyId("buc0AAAAMAAJ");
+////    GBRepository.getBooksByTitle("narnia");
+//    GBRepository.getBooksByAuthor("lewis");
 //}
+
 
 }
