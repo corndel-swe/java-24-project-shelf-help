04379ea3ba6ad7580263be9aec6ab2e5ec66a490
Anna Arnold <anna.arnold.96@gmail.com>
2024-11-04 11:48:20 +0000

    getBookbyid complete

--- src/main/java/org/project/shelfhelp/repositories/GBRepository.java
+++ src/main/java/org/project/shelfhelp/repositories/GBRepository.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kong.unirest.Unirest;
 import org.project.shelfhelp.models.Book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GBRepository {
     static String generalUrl = "https://books.google.com/books"; // Amend once you know the correct link
     static String apiKey = "AIzaSyDVEnipudTAkK1cFIw1Tuy7I4vRbqh8WUw"; // Don't think this is needed
@@ -44,10 +47,57 @@ public class GBRepository {
         System.out.println("rating: " + averagePublicRating);
 
 
-        return null;
+        return new Book(id, title, author, year, averagePublicRating, summary, bookCover);
     }
-public static void main(String[] args) throws Exception {
-    GBRepository.getABookbyId("buc0AAAAMAAJ");
-}
+
+//    public static List <Book> getBooksByTitle(String title) throws Exception {
+//
+//        String url = "https://www.googleapis.com/books/v1/volumes/?q=" + title;
+//
+//        var response = Unirest.get(url).header("Accept", "application.json").asString();
+//
+//        String book = response.getBody();
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        JsonNode bookTree = mapper.readTree(book);
+//
+////        // FOR DEBUGGING
+////        System.out.println(bookTree);
+//        var books = bookTree.get("items"); // Gets an individual book
+//        var individualBook = bookTree.get("items").get(2); // Gets an individual book
+//        List<Book> bookList = new ArrayList<>();
+//
+//        for (JsonNode i: books){
+//            JsonNode volumeInfo = bookTree.get("volumeInfo");
+//            JsonNode imageLinks = volumeInfo.get("imageLinks");
+//
+//            String id = String.valueOf(volumeInfo.get("id"));
+//            String author = String.valueOf(volumeInfo.get("authors"));
+//            int year = Integer.parseInt(String.valueOf(volumeInfo.get("publishedDate")).replace("\"",""));
+//            String summary = String.valueOf(volumeInfo.get("description"));
+//            String bookCover = String.valueOf(imageLinks.get("thumbnail"));
+//            Float averagePublicRating = Float.valueOf(String.valueOf(volumeInfo.get("averageRating")));
+//
+//
+//
+//            System.out.println("title: " + title);
+//            System.out.println("author: " + author);
+//            System.out.println("year: " + year);
+//            System.out.println("summary: " + summary);
+//            System.out.println("book cover: " + bookCover);
+//            System.out.println("rating: " + averagePublicRating);
+//
+//        }
+//
+//        System.out.println(books);
+//        System.out.println(individualBook);
+//
+//
+//        return null;
+//    }
+//public static void main(String[] args) throws Exception {
+//    GBRepository.getABookbyId("buc0AAAAMAAJ");
+//    GBRepository.getBooksByTitle("Alice in borderland");
+//}
 
 }
