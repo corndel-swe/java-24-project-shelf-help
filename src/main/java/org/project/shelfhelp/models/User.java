15fbaf279c2e22cda34c52c488ba75aacd4fa37d
kaiclastic <halajash567@outlook.com>
2024-11-07 15:17:11 +0000

    get stats and user profile

--- src/main/java/org/project/shelfhelp/models/User.java
+++ src/main/java/org/project/shelfhelp/models/User.java
@@ -65,4 +65,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                '}';
+    }
 }
